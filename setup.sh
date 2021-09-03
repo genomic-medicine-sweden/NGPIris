06875d64246123934551637e18d6d2f27bf52a0e
sylvinite <i.sylvin@outlook.com>
2021-09-03 12:16:33 +0200

    Added conda autoinstaller

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+SCRIPT=$(readlink -f $0)
+scriptdir=`dirname $SCRIPT`
+NAME=$1
+
+echo "INFO - Unloading active environment"
+conda info| tac | tac | grep -q $NAME && source deactivate || :
+echo "INFO - Removing existing instance of environment (if present)"
+conda remove -y -n $NAME --all || :
+
+echo "INFO - Creating new environment"
+conda create --yes --name $NAME python=3.7
+source activate $NAME
+
+echo "INFO - Installing requirements"
+conda config --add channels bioconda
+pip install -r ${scriptdir}/../../requirements.txt
+
+echo "INFO - Installing HCPinterface"
+pip install .
+echo "INFO - Installation complete!"
