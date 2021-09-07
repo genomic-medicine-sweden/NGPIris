1cc8ff0b0929b916583c1de32ddd33c2dfb5361e
sylvinite <i.sylvin@outlook.com>
2021-09-07 09:32:25 +0200

    Added keyfile support for creds

--- HCPInterface/cli/base.py
+++ HCPInterface/cli/base.py
@@ -6,25 +6,31 @@ from HCPInterface.hcp import HCPManager
 from HCPInterface.cli.functions import check, download, delete, search, upload
 
 @click.group()
-@click.option("-ep","--endpoint",help="Endpoint URL",type=str)
-@click.option("-id","--access_key_id",help="Amazon key identifier",type=str)
-@click.option("-key","--access_key",help="Amazon secret access key",type=str)
+@click.option("-ep","--endpoint",help="Endpoint URL",type=str,default="")
+@click.option("-id","--access_key_id",help="Amazon key identifier",type=str,default="")
+@click.option("-key","--access_key",help="Amazon secret access key",type=str,default="")
+@click.option('-p',"--profile", help="File containing ep, id & key; instead of using the CLI.",type=str,default="")
 @click.option("-b","--bucket",help="Bucket name",type=str, required=True)
 @click.option("--skip-hci", default=False, help="Skips calling the HCI. Requires --index & --password",is_flag=True)
 @click.option("-i", "--index", type=str,default="",help="Specify index from HCI to parse")
 @click.option("-pw","--password",default="",help="File with HCI password")
 @click.version_option(version)
 @click.pass_context
-def root(ctx, endpoint, access_key_id, access_key, bucket, skip_hci, index, password):
+def root(ctx, endpoint, access_key_id, access_key, bucket, skip_hci, index, password, profile):
     """HCP interfacing tool"""
+    #Invalid input checks
+    if not skip_hci and (index == "" or password == ""):
+        log.error("HCI requires an --index and --password")
+        sys.exit(-1)
+
+    if profile != "" and (endpoint != "" or access_key_id != "" or access_key != ""):
+        log.error("Credentials were provided both through a file and the CLI. Make up your mind")
+        sys.exit(-1)
+
     ctx.obj = {}
     hcpm = HCPManager(endpoint, access_key_id, access_key, bucket)
     hcpm.attach_bucket(bucket)
 
-    if not skip_hci and (index == "" or password == ""):
-        log.error("HCI requires an --index and --password")
-        sys.exit(-1)
-    
     if skip_hci:
         pass
     else:
--- tests/test_cmds.py
+++ tests/test_cmds.py
@@ -12,12 +12,63 @@ import mock
 import os
 import sys
 
-from HCPInterface import version, log
+from HCPInterface import version, log, WD
 from HCPInterface.cli.base import root
 
 from click.testing import CliRunner
 from distutils.sysconfig import get_python_lib
 from unittest.mock import patch, mock_open
+
+testWD = os.path.join(WD, '..', 'tests')
+credentials_path = os.path.join(testWD, 'keys.json')
+
+class MissingCredentialsError(Exception):
+    """Raise on trying to run tests without proper input of HCP credentials."""
+
+class TestProcess(unittest.TestCase):
+
+    def test01_upload_file(self):
+        self.assertIsNone(hcpm.upload_file(f"{ROOT_PATH}/data/test_reads_R1.fasterq", "unittest/test_reads_R1.fasterq"))
+
+    def test02_search_objects(self):
+        self.assertEqual(hcpm.search_objects("unittest/test_reads_R1.fasterq")[0].key, "unittest/test_reads_R1.fasterq")
+
+    def test03_download_file(self):
+        obj = hcpm.get_object("unittest/test_reads_R1.fasterq")
+        self.assertIsNone(hcpm.download_file(obj, "test_reads_R1.fasterq"))
+
+    def test04_md5_sha256(self):
+        remote_etag = hcpm.get_object("unittest/test_reads_R1.fasterq").e_tag
+        calculated_etag = calculate_etag(f"{ROOT_PATH}/data/test_reads_R1.fasterq")
+        self.assertEqual(calculated_etag, remote_etag)
+
+    def test05_delete_file(self):
+        obj = hcpm.get_object("unittest/test_reads_R1.fasterq")
+        self.assertIsNone(hcpm.delete_object(obj))
+        self.assertIsNone(hcpm.get_object("unittest/test_reads_R1.fasterq")) # Verify that object was removed
+
+    def test06_upload_error(self):
+        self.assertRaises(TypeError, lambda:hcpm.upload_file())
+
+    def test07_get_object_error(self):
+        self.assertRaises(TypeError, lambda:hcpm.get_object())
+
+    def test08_incorrect_credentials(self):
+        self.assertRaises(ValueError, lambda:HCPManager("x", "x", "x"))
+
+@pytest.fixture
+def hcpm():
+    with open(credentials_path, 'r') as inp:
+        credentials = json.load(inp)
+        endpoint = credentials['ep']
+        aws_access_key_id = credentials['aki']
+        aws_secret_access_key = credentials['sak']
+
+    if not all([endpoint, aws_access_key_id, aws_secret_access_key]):
+        raise MissingCredentialsError('One or more credentials missing from keys.json.')
+
+    hcpm = HCPManager(endpoint, aws_access_key_id, aws_secret_access_key)
+    hcpm.attach_bucket("ngs-test")
 
 @pytest.fixture
 def runner():
