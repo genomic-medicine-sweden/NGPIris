a9e60424ce8cc3c216d8968cbd76f908b1eefc4b
sylvinite <i.sylvin@outlook.com>
2021-09-28 09:59:35 +0200

    Config ini can now be found anywhere

--- HCPInterface/hcp/config.py
+++ HCPInterface/hcp/config.py
@@ -8,23 +8,13 @@ from distutils.sysconfig import get_python_lib
 def get_config():
     config = ConfigParser()
 
-    installed = False
-    for entry in os.listdir(get_python_lib()):
-        if 'HCPInterface-' in entry:
-             installed = True
-             break
+    foundpath = False
+    joiner = "" 
+    while not foundpath:
+        joiner = os.path.join(joiner, "..")
+        config_path=os.path.join(WD, joiner, 'HCPInterface', 'config.ini')
+        foundpath = os.path.isfile(config_path)
 
-    if installed:
-        #Also works but req conda
-        #config_path= os.path.join(os.path.expandvars("$CONDA_PREFIX"), "config.ini")
-
-        #pip show HCPInterface
-        #Backpedals all the way to the env folder
-        config_path = os.path.join(WD, '..', '..', '..', '..', 'config', 'config.ini')
-
-    else:
-        config_path = os.path.join(WD, '..', 'config.ini')
-  
     config.read(config_path)
 
     return config
