3c4736f60e06d571f824f5b28a27c25574dcaf88
sylvinite <i.sylvin@outlook.com>
2021-09-28 18:38:23 +0200

    Generalised bucket invocation for tests

--- tests/test_cmds.py
+++ tests/test_cmds.py
@@ -22,7 +22,7 @@ from unittest.mock import patch, mock_open
 testWD = os.path.join(WD, '..', 'tests')
 credentials_path = os.path.join(testWD, 'credentials.json')
 f1target =  os.path.join("unittest","test_reads_R1.fastq.gz") 
-
+bucket = "ngs-test"
 
 @pytest.fixture
 def runner():
@@ -38,42 +38,42 @@ def test_version(runner):
 ###
 
 def test_base(runner):
-    cmd = f"-b ngs-test -c {credentials_path}"
+    cmd = f"-b {bucket} -c {credentials_path}"
     res = runner.invoke(root, cmd.split())
     #Command should complain about lack of subcommand
     assert res.exit_code == 2
 
 def test_hci_base(runner):
-    cmd = f"-b ngs-test -c {credentials_path} hci"
+    cmd = f"-b {bucket} -c {credentials_path} hci"
     res = runner.invoke(root, cmd.split())
     assert res.exit_code == 0
 
 def test_upload(runner):
     source = os.path.join(testWD,"data","test_reads_R1.fastq.gz")
 
-    cmd = f"-b ngs-test -c {credentials_path} upload -i {source} -d {f1target} -m /tmp/meta.json --silent"
+    cmd = f"-b {bucket} -c {credentials_path} upload -i {source} -d {f1target} -m /tmp/meta.json --silent"
     #import pdb; pdb.set_trace()
     log.debug(cmd)
     res = runner.invoke(root, cmd.split())
     assert res.exit_code == 0
 
 def test_search(runner):
-    cmd = "-b ngs-test -c {0} search -q {1}".format(credentials_path, f1target)
+    cmd = f"-b {bucket} -c {credentials_path} search -q {f1target}"
     res = runner.invoke(root, cmd.split())
     assert res.exit_code == 0 
 
 def test_download(runner):
     dest =  os.path.join('tmp','tst.fq')
-    cmd = f"-b ngs-test -c {credentials_path} download -f -q {f1target} -d /{dest} --silent"
+    cmd = f"-b {bucket} -c {credentials_path} download -f -q {f1target} -d /{dest} --silent"
     log.debug(cmd)
     res = runner.invoke(root, cmd.split())
     assert res.exit_code == 0
 
 def test_delete(runner):
-    cmd1 = f"-b ngs-test -c {credentials_path} delete -q {f1target} -f"
+    cmd1 = f"-b {bucket} -c {credentials_path} delete -q {f1target} -f"
     res1 = runner.invoke(root, cmd1.split())
     #assert res.exit_code == 0
-    cmd2 = f"-b ngs-test -c {credentials_path} delete -q {os.path.join('unittest','meta.json')} -f"
+    cmd2 = f"-b {bucket} -c {credentials_path} delete -q {os.path.join('unittest','meta.json')} -f"
     res2 = runner.invoke(root, cmd2.split())
     assert res1.exit_code == 0
     assert res2.exit_code == 0
