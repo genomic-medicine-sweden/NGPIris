bd7c692fdd5716b611bb2b899b60b7ddb95e9d0b
sylvinite <i.sylvin@outlook.com>
2022-03-02 10:58:27 +0100

    Revised simpleconnection

--- tests/simpleConnectionTest.py
+++ tests/simpleConnectionTest.py
@@ -1,11 +1,16 @@
 from NGPIris import WD
 from NGPIris.hcp import HCPManager
+import argparse
 import pdb
 
+parser = argparse.ArgumentParser()
+parser.add_argument("bucket",help="Bucket name")
+args = parser.parse_args()
+
 #pdb.set_trace()
-print(f"WD at {WD}")
-hcpm = HCPManager(credentials_path="./credentials.json", autotest=False)
-hcpm.set_bucket('ngs-test')
+print(f"Working Directory at {WD}")
+hcpm = HCPManager(credentials_path="./credentials.json")
+hcpm.set_bucket(args.bucket)
 ls = hcpm.list_buckets()
 print(f"Buckets: {ls}")
 hcpm.test_connection()
