1f2c3fc834b6a152dfaa9f1f8d061574d94e14f1
Erik Brink <erik.brink@gu.se>
2024-02-05 16:23:14 +0100

    Add template code for commands

---
+++ cli/commands.py
@@ -0,0 +1,25 @@
+import click
+import logging
+
+@click.command()
+def delete() -> None:
+    pass
+
+@click.command()
+def download() -> None:
+    pass
+
+@click.command()
+def search() -> None:
+    pass
+
+@click.command()
+def upload() -> None:
+    pass
+
+
+def main() -> None:
+    pass
+
+if __name__ == "__main__":
+    main()
