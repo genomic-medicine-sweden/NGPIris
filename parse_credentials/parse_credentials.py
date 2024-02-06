14b159cd2176354882e1730439bb9bd7a2d940e8
Erik Brink <erik.brink@gu.se>
2024-02-06 09:43:04 +0100

    Update parse_credentials.py

--- parse_credentials/parse_credentials.py
+++ parse_credentials/parse_credentials.py
@@ -1,6 +1,18 @@
 
 import json
 
+class CredentialsHandler:
+    def __init__(self, credentials_path : str) -> None:
+
+        self.hcp : dict[str, str] = {}
+        self.hci : dict[str, str] = {}
+
+        credentials : dict[str, str] = parse_credentials(credentials_path)
+        for key, value in credentials.items():
+            setattr(self, key, value)
+
+
+
 def parse_credentials(credentials_path : str) -> dict[str,str]:
     credentials : dict[str, str] = {}
     with open(credentials_path, 'r') as inp:
