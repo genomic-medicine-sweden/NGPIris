420269a7c75d6a0ff01bb91970517ff67572a089
Erik Brink <erik.brink@gu.se>
2024-02-08 14:11:29 +0100

    Add method for setting ACL permissions given a User ID

--- NGPIris2/hcp/hcp.py
+++ NGPIris2/hcp/hcp.py
@@ -8,6 +8,7 @@ from typing import Any, List
 import os
 import json
 import parse
+import requests
 
 
 class HCPHandler:
@@ -23,10 +24,10 @@ class HCPHandler:
 
         s3_config = Config(
             s3 = {
-                'addressing_style': 'path',
-                'payload_signing_enabled': True
+                "addressing_style": "path",
+                "payload_signing_enabled": True
             },
-            signature_version = 's3v4'
+            signature_version = "s3v4"
         )
 
         self.s3_client = boto3.client(
@@ -38,10 +39,17 @@ class HCPHandler:
             config = s3_config
         )
 
+        self.iam_client = boto3.client(
+            "iam", 
+            aws_access_key_id = self.aws_access_key_id, 
+            aws_secret_access_key = self.aws_secret_access_key,
+            endpoint_url = self.endpoint
+        )
+
         self.transfer_config = TransferConfig(
-            multipart_threshold = ini_config.getint('hcp', 'size_threshold'),
-            max_concurrency = ini_config.getint('hcp', 'max_concurrency'),
-            multipart_chunksize = ini_config.getint('hcp', 'chunk_size')
+            multipart_threshold = ini_config.getint("hcp", "size_threshold"),
+            max_concurrency = ini_config.getint("hcp", "max_concurrency"),
+            multipart_chunksize = ini_config.getint("hcp", "chunk_size")
         )
     
     def mount_bucket(self, bucket_name : str) -> None:
@@ -190,6 +198,22 @@ class HCPHandler:
         # self.s3_client.put_object_acl()
         pass
 
-    def add_bucket_acl(self):
-        # self.s3_client.put_bucket_acl()
-        pass
+    def add_bucket_acl(self, user_ID : str, permission : str ) -> None:
+        if not permission in ["FULL_CONTROL", "WRITE", "WRITE_ACP", "READ", "READ_ACP"]:
+            print("Invalid permission option")
+            exit()
+        access_control_policy : dict = {
+            "Grants" : [
+                {
+                    "Grantee": {
+                        "ID": user_ID,
+                        "Type": "CanonicalUser"
+                    },
+                    "Permission": permission
+                }
+            ]
+        }
+        self.s3_client.put_bucket_acl(
+            Bucket = self.bucket_name,
+            AccessControlPolicy = access_control_policy
+        )
