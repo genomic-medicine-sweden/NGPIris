80928b36713c2d363b245619e8877f0d6181bfd4
Erik Brink <erik.brink@gu.se>
2024-02-13 11:05:32 +0100

    Add HCI SQL querying

--- NGPIris2/hci/hci.py
+++ NGPIris2/hci/hci.py
@@ -2,6 +2,7 @@
 import NGPIris2.parse_credentials.parse_credentials as pc
 import requests
 import json
+import pandas as pd
 
 # TO BE REMOVED LATER
 import urllib3
@@ -74,3 +75,41 @@ class HCIHandler:
             # To-Do: Add exception handling
         
         return response.json()
+    
+    def SQL_query(self, query_path : str) -> pd.DataFrame:
+        with open(query_path, "r") as inp:
+            url     : str            = "https://" + self.address + ":" + self.api_port + "/api/search/query/sql/"
+            query   : dict[str, str] = json.load(inp)
+            headers : dict[str, str] = {
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+                "Authorization": "Bearer " + self.token
+            }
+        response : requests.Response = requests.post(
+            url, 
+            json.dumps(query), 
+            headers=headers, 
+            verify = False
+        )
+
+        if response.status_code != 200:
+            print(response.text)
+            # To-Do: Add exception handling
+
+        result_list = list(response.json()["results"])
+        result_df : pd.DataFrame = pd.DataFrame(result_list)
+        meta_df : pd.DataFrame = pd.DataFrame()
+
+        for row in result_df["metadata"]:
+            metadata_dict : dict = dict(row)
+            df = pd.DataFrame(metadata_dict)
+            meta_df = pd.concat([meta_df, df])
+
+        meta_df = meta_df.reset_index(drop = True)
+
+        for col in meta_df.columns:
+            result_df.insert(len(result_df.columns), col, meta_df[col], allow_duplicates = True)
+
+        result_df = result_df.drop("metadata", axis = 1)
+
+        return result_df
