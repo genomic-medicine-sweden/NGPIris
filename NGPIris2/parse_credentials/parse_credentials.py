12b3c0415f6531f76b6a5c91b983a7a548d49bdb
Erik Brink <erik.brink@gu.se>
2024-02-14 15:44:46 +0100

    Handle incomplete credentials

--- NGPIris2/parse_credentials/parse_credentials.py
+++ NGPIris2/parse_credentials/parse_credentials.py
@@ -1,23 +1,32 @@
 
 import json
 
+Credentials = dict[str, dict[str, str]]
 class CredentialsHandler:
     def __init__(self, credentials_path : str) -> None:
 
         self.hcp : dict[str, str] = {}
         self.hci : dict[str, str] = {}
 
-        credentials : dict[str, str] = parse_credentials(credentials_path)
+        credentials : Credentials = parse_credentials(credentials_path)
         for key, value in credentials.items():
             setattr(self, key, value)
 
+def check_empty_field(credentials : Credentials):
+    empty_fields = []
+    for k1, d in credentials.items():
+        for k2, v in d.items():
+            if v == "":
+                empty_fields.append("- " + k2 + "\n")
+    
+    if empty_fields:
+        raise RuntimeError("Missing entry for the following fields in the credentials file: \n" + ''.join(empty_fields))
 
-
-def parse_credentials(credentials_path : str) -> dict[str,str]:
-    credentials : dict[str, str] = {}
+def parse_credentials(credentials_path : str) -> Credentials:
+    credentials : Credentials = {}
     with open(credentials_path, 'r') as inp:
-        credentials = json.load(inp)
+        credentials : Credentials = json.load(inp)
         
-        # Raise exceptions for incomplete credentials here
+        check_empty_field(credentials)
 
         return credentials
