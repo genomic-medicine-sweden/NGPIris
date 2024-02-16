8bfba70626dbafb417d677de6f1f653c30283f59
Erik Brink <erik.brink@gu.se>
2024-02-16 11:32:17 +0100

    Add ability to mutually exlude credentials

--- NGPIris2/parse_credentials/parse_credentials.py
+++ NGPIris2/parse_credentials/parse_credentials.py
@@ -12,15 +12,31 @@ class CredentialsHandler:
         for key, value in credentials.items():
             setattr(self, key, value)
 
+def all_fields_empty(key : str, credentials : Credentials) -> bool:
+    return all([v == "" for v in credentials[key].values()])
+
 def check_empty_field(credentials : Credentials):
-    empty_fields = []
+    empty_fields_per_entry : dict[str, list[str]] = {}
     for k1, d in credentials.items():
+        if all_fields_empty(k1, credentials):
+            continue
+        empty_fields : list[str] = []
         for k2, v in d.items():
             if v == "":
-                empty_fields.append("- " + k1 + " : " + k2 + "\n")
-    
-    if empty_fields:
-        raise RuntimeError("Missing entry for the following fields in the credentials file: \n" + "".join(empty_fields))
+                empty_fields.append("\t- " + k2 + "\n")
+        if empty_fields:
+            empty_fields_per_entry[k1] = empty_fields
+
+    all_empty_fields = []
+    for entry, l in empty_fields_per_entry.items():
+        l.insert(0, "- " + entry + ":\n")
+        all_empty_fields.append("".join(l))
+
+    if all_empty_fields:
+        raise RuntimeError(
+            "Missing fields for the following entries in the credentials file: \n" + 
+            "".join(all_empty_fields)
+        )
 
 def parse_credentials(credentials_path : str) -> Credentials:
     credentials : Credentials = {}
