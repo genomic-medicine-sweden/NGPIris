6837b9b9f488fc2cd3566283799e88d305bade2c
Erik Brink <erik.brink@gu.se>
2024-02-16 10:21:52 +0100

    Add docstrings to HCI

--- NGPIris2/hci/hci.py
+++ NGPIris2/hci/hci.py
@@ -7,7 +7,16 @@ import pandas as pd
 import urllib3
 
 class HCIHandler:
+    """Class for handling HCI requests."""
     def __init__(self, credentials_path : str, use_ssl : bool = False) -> None:
+        """
+        Constructor for the HCIHandler class.
+
+        :param credentials_path: Path to the JSON credentials file
+        :type credentials_path: str
+        :param use_ssl: Boolean choice between using SSL, defaults to False
+        :type use_ssl: bool, optional
+        """
         credentials_handler = pc.CredentialsHandler(credentials_path)
         self.hci = credentials_handler.hci
         self.username = self.hci["username"]
@@ -23,6 +32,14 @@ class HCIHandler:
             urllib3.disable_warnings()
     
     def request_token(self) -> None:
+        """
+        Request a token from the HCI, which is stored in the HCIHandler object. 
+        The token is used for every operation that needs to send a request to 
+        HCI.
+
+        :raises RuntimeError: If there was a problem when requesting a token, a 
+        runtime error will be raised 
+        """
         url = "https://" + self.address + ":" + self.auth_port + "/auth/oauth/"
         data = {
             "grant_type": "password", 
@@ -36,17 +53,32 @@ class HCIHandler:
         try:
             response : requests.Response = requests.post(url, data = data, verify = self.use_ssl)
         except: 
-            error_msg : str = "The token reqeust made at " + url + " failed. Please check your connection and that you have your VPN enabled"
+            error_msg : str = "The token request made at " + url + " failed. Please check your connection and that you have your VPN enabled"
             raise RuntimeError(error_msg) from None
 
         token : str = response.json()["access_token"]
         self.token = token
 
     def list_index_names(self) -> list[str]:
+        """
+        Retrieve a list of all index names.
+
+        :return: A list of index names
+        :rtype: list[str]
+        """
         response : requests.Response = h.get_index_response(self.address, self.api_port, self.token, self.use_ssl)
         return [entry["name"]for entry in response.json()]
     
     def look_up_index(self, index_name : str) -> dict:
+        """
+        Look up index information in the form of a dictionary by submitting 
+        the index name. Will return an empty dictionary if no index was found.
+
+        :param index_name: The index name
+        :type index_name: str
+        :return: A dictionary containing information about an index
+        :rtype: dict
+        """
         response : requests.Response = h.get_index_response(self.address, self.api_port, self.token, self.use_ssl)
 
         for entry in response.json():
@@ -57,9 +89,29 @@ class HCIHandler:
         
 
     def query(self, query_path : str) -> dict:
+        """
+        Make query to an HCI index. Will return a response in the shape of a 
+        dictionary.
+
+        :param query_path: Path to the query JSON file
+        :type query_path: str
+        :return: A dictionary containing the response from the query
+        :rtype: dict
+        """
         return h.get_query_response(query_path, self.address, self.api_port, self.token, self.use_ssl).json()
     
     def SQL_query(self, query_path : str) -> pd.DataFrame:
+        """
+        Perform an SQL query given a path to a JSON file containing the 
+        query. Returns a DataFrame containing the result of the query. 
+
+        :param query_path: Path to the query JSON file
+        :type query_path: str
+        :raises RuntimeError: Will raise a runtime error if an error was found 
+        with the SQL query
+        :return: A DataFrame containing the result of the SQL query
+        :rtype: pd.DataFrame
+        """
         response = h.get_query_response(query_path, self.address, self.api_port, self.token, self.use_ssl, "sql/")
 
         result_list = list(response.json()["results"])
--- NGPIris2/hci/helpers.py
+++ NGPIris2/hci/helpers.py
@@ -3,10 +3,33 @@ import json
 import requests
 
 def raise_request_error(response : requests.Response, url : str) -> None:
-    error_msg : str = "The response code from the reqeust made at " + url + " returned status code " + str(response.status_code) + ": " + str(json.loads(response.text)["errorMessage"])
+    """
+    Raise a request error.
+
+    :param response: The response containing the error to be raised
+    :type response: requests.Response
+    :param url: The URL where the request was made
+    :type url: str
+    :raises RuntimeError: Will raise a runtime error for a request
+    """
+    error_msg : str = "The response code from the request made at " + url + " returned status code " + str(response.status_code) + ": " + str(json.loads(response.text)["errorMessage"])
     raise RuntimeError(error_msg) from None
 
 def get_index_response(address : str, api_port : str, token : str, use_ssl : bool) -> requests.Response:
+    """
+    Retrieve the index response given the address, API port and token.
+
+    :param address: The address where request is to be made
+    :type address: str
+    :param api_port: The API port at the given address
+    :type api_port: str
+    :param token: The HCI token 
+    :type token: str
+    :param use_ssl: Boolean choice of using SSL
+    :type use_ssl: bool
+    :return: A response containing information about the index
+    :rtype: requests.Response
+    """
     url     : str            = "https://" + address + ":" + api_port + "/api/search/indexes/"
     headers : dict[str, str] = {
         "Accept": "application/json",
@@ -25,6 +48,23 @@ def get_index_response(address : str, api_port : str, token : str, use_ssl : boo
     return response
 
 def get_query_response(query_path, address : str, api_port : str, token : str, use_ssl : bool, path_extension : str = "") -> requests.Response:
+    """
+    Retrieve the query response given the address, API port and token.
+
+    :param address: The address where request is to be made
+    :type address: str
+    :param api_port: The API port at the given address
+    :type api_port: str
+    :param token: The HCI token 
+    :type token: str
+    :param use_ssl: Boolean choice of using SSL
+    :type use_ssl: bool
+    :param path_extension: possibly extend the request URL. Used for example 
+    when making SQL requests. Defaults to ""
+    :type path_extension: str, optional
+    :return: A response containing information about the query
+    :rtype: requests.Response
+    """
     with open(query_path, "r") as inp:
         url     : str            = "https://" + address + ":" + api_port + "/api/search/query/" + path_extension
         query   : dict[str, str] = json.load(inp)
