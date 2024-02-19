d0684b90e51bcb4c8eaadeb65aeeb1ca1141d249
Erik Brink <erik.brink@gu.se>
2024-02-19 09:11:09 +0100

    Add requirements to `setup.py`

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
+try:
+    with open("requirements.txt", "r", encoding="utf-8") as f:
+        install_requires : list[str] = [x.strip() for x in f.readlines()]
+except IOError:
+    install_requires : list[str] = []
+
 setup(
-    name="iris2",
-    version="0.0.1",
-    packages=find_packages(),
+    name='NGPIris2',
+    version='0.0.1',
+    install_requires=install_requires,
+    packages = find_packages()
 )
