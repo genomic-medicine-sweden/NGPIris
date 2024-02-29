ba4020503243cae8503ff8b6ecd155b7d0631d55
Erik Brink <erik.brink@gu.se>
2024-02-29 15:48:46 +0100

    Update hcp.py

--- NGPIris2/hcp/hcp.py
+++ NGPIris2/hcp/hcp.py
@@ -34,7 +34,7 @@ class HCPHandler:
         """
         credentials_handler = pc.CredentialsHandler(credentials_path)
         self.hcp = credentials_handler.hcp
-        self.endpoint = self.hcp["endpoint"]
+        self.endpoint = "https://" + self.hcp["endpoint"]
         self.aws_access_key_id = self.hcp["aws_access_key_id"]
         self.aws_secret_access_key = self.hcp["aws_secret_access_key"]
         self.token = self.aws_access_key_id + ":" + self.aws_secret_access_key
