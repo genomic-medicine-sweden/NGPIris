e9902ef900893b9418fa893ba0694ea30f11ab47
Erik Brink <erik.brink@gu.se>
2024-03-12 09:09:46 +0100

    Structure imports init

--- NGPIris2/hcp/__init__.py
+++ NGPIris2/hcp/__init__.py
@@ -1,1 +1,9 @@
 from NGPIris2.hcp.hcp import HCPHandler
+from NGPIris2.hcp.acl import (
+    get_object_acl,
+    get_bucket_acl,
+    add_single_object_acl,
+    add_single_bucket_acl,
+    add_object_acl,
+    add_bucket_acl
+)
