e2117143b2c12687059b2c55f2c5948636e14215
Erik Brink <erik.brink@gu.se>
2024-03-25 15:08:10 +0100

    Add simple list buckets command

--- NGPIris2/cli/__init__.py
+++ NGPIris2/cli/__init__.py
@@ -1,9 +1,15 @@
 
 import click
+from click.core import Context, Argument, Option
+
+from NGPIris2.hcp import HCPHandler
 
 @click.group()
-def cli():
-    pass
+@click.argument("credentials")
+@click.pass_context
+def cli(context : Context, credentials : str):
+    context.ensure_object(dict)
+    context.obj["hcph"] = HCPHandler(credentials)
 
 @cli.command()
 def upload():
@@ -11,4 +17,10 @@ def upload():
 
 @cli.command()
 def download():
+@cli.command()
+@click.pass_context
+def list_buckets(context : Context):
+    hcph : HCPHandler = context.obj["hcph"]
+    click.echo(hcph.list_buckets())
+
     pass
