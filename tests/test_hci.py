c4b449a50e3dcddabe68a354919614ba2b02b471
Erik Brink <erik.brink@gu.se>
2025-05-30 14:06:22 +0200

    Update test_hci.py

--- tests/test_hci.py
+++ tests/test_hci.py
@@ -1,73 +1,47 @@
 
-from configparser import ConfigParser
-from NGPIris.hci import HCIHandler
+from conftest import CustomConfig
 from random import randint
 from json import dump
 from os import remove
 
-ini_config = ConfigParser()
-ini_config.read("tests/test_conf.ini")
-
-hci_h = HCIHandler(ini_config.get("General", "credentials_path"))
-hci_h.request_token()
-
-def test_list_index_names_type() -> None:
-    list_of_indexes = hci_h.list_index_names()
+def test_list_index_names_type(custom_config : CustomConfig) -> None:
+    custom_config.hci_h.request_token()
+    list_of_indexes = custom_config.hci_h.list_index_names()
     assert type(list_of_indexes) is list
 
-def test_look_up_all_indexes() -> None:
-    list_of_indexes = hci_h.list_index_names()
+def test_look_up_all_indexes(custom_config : CustomConfig) -> None:
+    custom_config.hci_h.request_token()
+    list_of_indexes = custom_config.hci_h.list_index_names()
     for index in list_of_indexes:
-        assert hci_h.look_up_index(index)
+        assert custom_config.hci_h.look_up_index(index)
 
-def test_fail_index_look_up() -> None:
-    assert not hci_h.look_up_index("anIndexThatDoesNotExist")
+def test_fail_index_look_up(custom_config : CustomConfig) -> None:
+    custom_config.hci_h.request_token()    
+    assert not custom_config.hci_h.look_up_index("anIndexThatDoesNotExist")
     
-def test_make_simple_raw_query() -> None:
-    list_of_indexes = hci_h.list_index_names()
+def test_make_simple_raw_query(custom_config : CustomConfig) -> None:
+    custom_config.hci_h.request_token()
+    list_of_indexes = custom_config.hci_h.list_index_names()
     arbitrary_index = list_of_indexes[randint(0, len(list_of_indexes) - 1)]
-    result = hci_h.raw_query(
+    result = custom_config.hci_h.raw_query(
         {
             "indexName" : arbitrary_index
         }
     )
     assert result["indexName"] == arbitrary_index
 
-def test_raw_query_with_results() -> None:
-    query = {
-        "indexName" : "Parsed_VCF_Index",
-        "facetRequests" : [
-            {
-                "fieldName" : "ref"
-            },
-            {
-                "fieldName" : "alt"
-            }
-        ]
-    }
-    parsed_VCF_index_result : list = hci_h.raw_query(query)["facets"]
-    for i in range(len(parsed_VCF_index_result)):
-        assert parsed_VCF_index_result[i]["termCounts"] # Assert if the result is not empty
-
-def test_fail_raw_query() -> None:
+def test_fail_raw_query(custom_config : CustomConfig) -> None:
+    custom_config.hci_h.request_token()
     query = {}
     try:
-        hci_h.raw_query(query)
+        custom_config.hci_h.raw_query(query)
     except:
         assert True
     else: # pragma: no cover
         assert False
 
-def test_make_simple_raw_query_from_JSON() -> None:
-    list_of_indexes = hci_h.list_index_names()
-    arbitrary_index = list_of_indexes[randint(0, len(list_of_indexes) - 1)]
-    path = "tests/data/json_test_query.json"
-    with open(path, "w") as f:
-        query = {
-            "indexName": arbitrary_index
-        }
-        dump(query, f, indent = 4)
-    result = hci_h.raw_query_from_JSON(path)
-    assert result["indexName"] == arbitrary_index
-    remove(path)    
-    
+def test_make_query(custom_config : CustomConfig):
+    custom_config.hci_h.request_token()
+    custom_config.hci_h.query(
+        custom_config.test_index
+    )
