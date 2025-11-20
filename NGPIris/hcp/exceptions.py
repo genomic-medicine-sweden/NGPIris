edf5890d24a895a9582981025755302dd08eea81
Erik Brink <erik.brink@gu.se>
2025-11-20 11:34:30 +0100

    Update exceptions.py

--- NGPIris/hcp/exceptions.py
+++ NGPIris/hcp/exceptions.py
@@ -1,36 +1,80 @@
 
-class NoBucketMounted(Exception):
-    pass
-
-class BucketNotFound(Exception):
-    pass
-
-class BucketForbidden(Exception):
-    pass
-
-class ObjectAlreadyExist(Exception):
-    pass
-
-class ObjectDoesNotExist(Exception):
-    pass
-
-class DownloadLimitReached(Exception):
-    pass
-
-class NotADirectory(Exception):
-    pass
+# -------------- Parsing exceptions --------------
 
 class NotAValidTenant(Exception):
+    """
+    The given tenant name is not valid.
+    """
     pass
 
 class UnableToParseEndpoint(Exception):
+    """
+    The given endpoint URL was not correctly parsed.
+    """
     pass
 
-class UnallowedCharacter(Exception):
+# -------------- Bucket exceptions --------------
+class NoBucketMounted(Exception):
+    """"
+    No bucket has been mounted before using a method that require it.
+    """
+    pass
+
+class BucketNotFound(Exception):
+    """
+    The bucket could not be found.
+    """
+    pass
+
+class BucketForbidden(Exception):
+    """
+    The credentials used do not have permission to reach this bucket.
+    """
+    pass
+
+# -------------- Bucket object exceptions --------------
+
+class ObjectAlreadyExist(Exception):
+    """
+    The object already exist on the mounted bucket.
+    """
+    pass
+
+class ObjectDoesNotExist(Exception):
+    """
+    The object does not exist on the mounted bucket.
+    """
     pass
 
 class IsFolderObject(Exception):
+    """
+    The object on the mounted bucket is a folder.
+    """
     pass
 
 class SubfolderException(Exception):
+    """
+    There is at least one subfolder in the given path on the mounted bucket.
+    """
+    pass
+
+class DownloadLimitReached(Exception):
+    """
+    Download limit was reached while downloading file objects from the
+    mounted bucket.
+    """
+    pass
+
+# -------------- File system exceptions --------------
+
+class NotADirectory(Exception):
+    """
+    The given file system path is not a directory
+    """
+    pass
+
+class UnallowedCharacter(Exception):
+    """
+    A character that is not allowed was used.
+    """
     pass
