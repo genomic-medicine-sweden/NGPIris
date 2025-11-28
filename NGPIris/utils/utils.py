f8ca260528ca499df280c97c9ae33489e7e84bd1
Erik Brink <erik.brink@gu.se>
2025-11-28 14:03:17 +0100

    Update utils.py

--- NGPIris/utils/utils.py
+++ NGPIris/utils/utils.py
@@ -3,8 +3,14 @@ from hashlib import md5
 
 
 def base64_hashing(string: str) -> str:
+    """
+    Hash `string` to a base64 string
+    """
     return (b64encode(string.encode("ascii"))).decode("UTF-8")
 
 
 def md5_hashing(string: str) -> str:
-    return md5(string.encode("ascii")).hexdigest()
+    """
+    Hash `string` to a md5 string
+    """
+    return md5(string.encode("ascii")).hexdigest() #noqa: S324
