2080a4df12daf6370370bd96fd8eba5686c3dafd
Erik Brink <erik.brink@gu.se>
2026-08-24 11:59:47 +0200

    Revert "Update sections.py"
    This reverts commit 0b4c28a5202e8145bcd85883feada5bed4980ccd.

--- NGPIris/cli/sections.py
+++ NGPIris/cli/sections.py
@@ -48,8 +48,12 @@ class SectionedGroup(click.Group):
         """
         Override Group.command to accept a 'section' kwarg.
 
+        Example:
         ::
             @cli.command(section="User Commands")
+            def add():
+                ...
+
         """
         section = kwargs.pop("section", None)
 
