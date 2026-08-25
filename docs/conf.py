1bb356e64bbfb8eae8a74d66665c2d5be09171c9
Erik Brink <erik.brink@gu.se>
2026-08-25 08:57:58 +0200

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ toc_object_entries_show_parents = "hide"
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = "furo"
+html_theme = "shibuya"
 html_static_path = ["_static"]
 html_logo = html_favicon = "_static/gms.png"
 
