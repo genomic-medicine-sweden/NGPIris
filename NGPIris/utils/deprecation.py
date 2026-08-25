7dcad9266e4939b2afa6872026d547cc2a73d49f
Erik Brink <erik.brink@gu.se>
2026-08-25 13:15:51 +0200

    Update deprecation.py

--- NGPIris/utils/deprecation.py
+++ NGPIris/utils/deprecation.py
@@ -7,66 +7,6 @@ from warnings import deprecated as deprecated_base
 if TYPE_CHECKING:
     from collections.abc import Callable
 
-"""
-def compose[**P, R](decorators: list[Callable[P, R]]) -> Callable[P, R]:
-    def decorator(f) -> R:
-        for decorator_ in reversed(decorators):
-            f = decorator_(f)
-        return f
-
-    return decorator
-
-def deprecated[**P, R](function: Callable[P, R]) -> Callable[P, R]:
-    @wraps(function)
-    @wraps(deprecated_base)
-    def decorator(*args: P.args, **kwargs: P.kwargs) -> R:
-        print("hahaha")
-        return function(*args, **kwargs)
-
-    return decorator
-"""
-
-"""
-def deprecated[**P, R](
-    deprecatation_message,
-    deprecated_in: str = "",
-    removed_in: str = "",
-) -> Callable[..., Callable[P, R]]:  # -> Callable[P, Callable[P, R]]:
-    # f = deprecated_base(deprecatation_message)(function)
-    # decorator = partial(f)
-    # return decorator
-
-    def decorator(function: Callable[P, R]) -> Callable[P, R]:
-        # @wraps(function)
-        @wraps(deprecated_base(deprecatation_message))
-        def wrapper(*args: P.args, **kwargs: P.kwargs) -> R:
-            return function(*args, **kwargs)
-
-        return wrapper
-
-    return decorator
-"""
-
-"""
-def compose[**P, R](functions: list[Callable[P, R]]) -> Callable[P, R]:
-    def inner(args):
-        for function in reversed(functions):
-            args = function(args)
-        return args
-
-    return inner
-    # return reduce(lambda x, y: x, functions)
-
-
-def compose[R](*functions: Callable[..., R]) -> Callable[..., R]:
-    def inner(args):
-        for f in reversed(functions):
-            args = f(args)
-        return args
-
-    return inner
-"""
-
 
 def add_deprecation_in_doc_string[**P, R](
     function: Callable[P, R],
